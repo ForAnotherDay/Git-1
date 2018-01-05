5672f2a542019982725d8bcd104b99d9e63c953b
FANNTASY20 <2429782582@qq.com>
2018-01-05 16:45:57 +0800

    新建项目

---
+++ src/com/zhiyou100/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.zhiyou100.git;
+
+/**  
+* @ClassName: GitTest  
+* @Description: TODO
+* @author Administrator  
+* @date 2018年1月5日  下午4:41:45
+*  
+*/
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
